463fc9179fde66548577553ef7df56622dde09d5
dan <dan.traczynski@gmail.com>
2013-12-02 00:22:22 +0000

    initial revision

---
+++ src/main/javascript/AddressGadget.js
@@ -0,0 +1,216 @@
+// indexOf does not exist in IE8 and earlier
+Array.prototype.hasObject = (
+  !Array.indexOf ? function (o)
+  {
+    var l = this.length + 1;
+    while (l -= 1)
+    {
+        if (this[l - 1] === o)
+        {
+            return true;
+        }
+    }
+    return false;
+  } : function (o)
+  {
+    return (this.indexOf(o) !== -1);
+  }
+);
+
+function AddressGadget() {}
+// postalCode, countryCode must be set in addr
+AddressGadget.prototype.lookupPostalCode = function(addr, callbacks) {}
+AddressGadget.prototype.lookupFullAddress = function(addr, callbacks) {}
+// Performs a callback to the given callback function name in a structure of callbacks for the given arguments.
+// callbacks: structure of callbacks
+// fnName: name to look up in the callbacks
+// args: a dynamic list of arguments to pass to the function
+AddressGadget.prototype.doCallback = function(callbacks, fnName, args) {
+	if (callbacks.hasOwnProperty(fnName)) {
+		callbacks[fnName].apply(this, Array.prototype.slice.call(arguments, 2));
+	}
+}
+
+function AddressGadgetGMap() {
+	AddressGadget.call(this);
+	this.geocoder = new google.maps.Geocoder();
+	this.routeInPCode = ['ca','uk']; // countries with pcodes that identify a very small set of addresses
+}
+AddressGadgetGMap.prototype = AddressGadget.prototype;
+
+// Some countries have postal codes that are specific to a street while others cover large areas
+AddressGadgetGMap.prototype.includeRoute = function(countryCode) {
+	return this.routeInPCode.indexOf(countryCode) >= 0;
+}
+
+// ac: a value in the address_components[] array
+AddressGadgetGMap.prototype.isAddrComponentOfType = function(ac, type) {
+	for (var j = 0; j < ac.types.length; j++) {
+		if (ac.types[j] === type) {
+			return true;
+		}
+	}
+	return false;
+}
+AddressGadgetGMap.prototype.findResult = function(results, desiredType, testfn) {
+	var arr = [];
+	var bestguess = null;
+	for (var i = 0; i < results.length; i++) {
+		var addr_components = results[i].address_components;
+		for (var j = 0; j < addr_components.length; j++) {
+			var ac = addr_components[j];
+			if (this.isAddrComponentOfType(ac, desiredType)) {
+				// this address component matches the inputs
+				if (bestguess == null) {
+					bestguess = results[i];
+				}
+				if (testfn(ac)) {
+					return results[i];
+				}
+			}
+		}
+	}
+	return bestguess;
+}
+// param 1: addr_components
+// param 2: property to return
+// param 3..n: desiredType
+AddressGadgetGMap.prototype.getAddressComponent = function(addr_components, prop, desiredType) {
+	var addr_components = arguments[0];
+	var prop = arguments[1];
+	for (var i = 0; i < addr_components.length; i++) {
+		var ac = addr_components[i];
+		for (var j = 2; j < arguments.length; j++) {
+			if (this.isAddrComponentOfType(ac, arguments[j])) {
+				return ac[prop];
+			}
+		}
+	}
+	return null;
+}
+AddressGadgetGMap.prototype.equalsIgnoreCaseWithRegex = function (s1, s2, replace, value, firstN) {
+	if (!s1 || !s2) return false;
+	s1 = s1.toUpperCase().replace(replace, value);
+	s2 = s2.toUpperCase().replace(replace, value);
+	if (firstN) {
+		s1 = s1.slice(0, firstN);
+		s2 = s2.slice(0, firstN);
+	}
+	return s1 == s2;
+}
+// google gets a lot of Canadian postal codes wrong, so only match on the first 3 characters
+AddressGadgetGMap.prototype.firstN = function(countryCode) {
+	if (countryCode === 'ca') { return 3; }
+	return 0;
+}
+AddressGadgetGMap.prototype.toAddr = function(addr, res, includeRoute) {
+	var acArr = res.address_components;
+	var latlon = res.geometry.location;
+	var p = this.getAddressComponent(acArr, 'long_name', 'postal_code');
+	if (!this.equalsIgnoreCaseWithRegex(addr.postalCode, p, /[\s-]+/g, '', 0)) {
+		// if the returned postal code doesn't match the input, don't overwrite the user's input
+		p = addr.postalCode.toUpperCase();
+	}
+	var fLong = function(ac) { return ac.long_name; }
+	var fShort = function(ac) { return ac.short_name; }
+	var o = {
+		lat: latlon.lat(),
+		lon: latlon.lng(),
+		locality: this.getAddressComponent(acArr, 'long_name', 'postal_town', 'locality'),
+		tlaal: this.getAddressComponent(acArr, 'long_name', 'administrative_area_level_1'),
+		tlaas: this.getAddressComponent(acArr, 'short_name', 'administrative_area_level_1'),
+		postalCode: p,
+		countryCode: addr.countryCode,
+		accuracy: res.geometry.location_type,
+		debug: res
+	};
+	if (includeRoute) {
+		o.streetNumber = this.getAddressComponent(acArr, 'long_name', 'street_number');
+		o.route = this.getAddressComponent(acArr, 'long_name', 'route');
+		o.neighborhood = this.getAddressComponent(acArr, 'long_name', 'neighborhood');
+	}
+	return o;
+}
+AddressGadgetGMap.prototype.lookupPostalCode = function(addr, callbacks) {
+	var t = this;
+	var v = addr.postalCode + ", " + addr.countryCode;
+	var ms = Date.now();
+	// first transform the postal code to lat/lon
+	t.geocoder.geocode(
+		{'address': v},
+		function(results, status) {
+			var res = {
+				op: 'lookupPostalCode',
+				server: 'google',
+				exec_time: Date.now() - ms,
+				serverResponseCode: status
+			};
+			var svrRes = results;
+			if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+				// TODO: UK: WC2H 9LC produces error ZERO_RESULTS
+				res.code = 'NO_MATCH';
+				t.doCallback(callbacks, 'onNoMatch', res);
+			} else if (status == google.maps.GeocoderStatus.OK) {
+				var latlon = results[0].geometry.location; // TODO: why 0?
+				// second perform a reverse geocode lookup to identify the most granular address possible
+				t.geocoder.geocode(
+					{'latLng': latlon},
+					function(results, status) {
+						res.exec_time = Date.now() - ms;
+						res.serverResponseCode = status;
+						svrRes = results;
+						if (status == google.maps.GeocoderStatus.OK) {
+							// find the address component matching the postal code
+							// use this address component to populate the suggestions (might be incomplete for bad input)
+							var dbg = t.findResult(results, 'postal_code', function(ac) {
+								// sometimes weird postal codes come back - this ensures only the desired one is used
+								var s = ac.long_name;
+								return t.equalsIgnoreCaseWithRegex(s, addr.postalCode, /[\s-]+/g, '', t.firstN(addr.countryCode));
+							});
+							res.code = 'SUCCESS';
+							t.doCallback(callbacks, 'onFullAddressMatch', res, t.toAddr(addr, dbg, t.includeRoute(addr.countryCode)));
+						} else {
+							res.code = 'ERROR';
+							res.msg = 'Reverse geocode lookup from postal code [' + v + '] and lat/lon [' + latlon + '] failed with status: ' + status;
+							t.doCallback(callbacks, 'onError', res);
+						}
+					}
+				);
+			} else {
+				res.code = 'ERROR';
+				res.msg = "Postal code lookup [" + v + "] failed with status: " + status;
+				t.doCallback(callbacks, 'onError', res);
+			}
+			t.doCallback(callbacks, 'logResult', res, svrRes);
+		}
+	);
+}
+AddressGadgetGMap.prototype.lookupFullAddress = function(addr, callbacks) {
+	var t = this;
+	var ms = Date.now();
+	t.geocoder.geocode(
+		{'address': addr.toString()},
+		function(results, status) {
+			var res = {
+				op: 'lookupFullAddress',
+				server: 'google',
+				exec_time: Date.now() - ms,
+				serverResponseCode: status
+			};
+			var addrOut = addr;
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results.length) {
+					res.code = 'SUCCESS';
+					addrOut = t.toAddr(addr, results[0], true);
+				} else {
+					res.code = 'NO_MATCH';
+				}
+			} else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+				res.code = 'NO_MATCH';
+			} else {
+				res.code = 'ERROR';
+			}
+			t.doCallback(callbacks, 'onFinish', res, addrOut);
+		}
+	);
+}
---
+++ src/test/javascript/AddressGadget_spec.js
@@ -0,0 +1,251 @@
+describe('AddressGadget', function() {
+    describe('doCallback', function() {
+        var callbacks = null;
+
+        beforeEach(function() {
+            callbacks = {
+                fn: function(a, b) {
+                }
+            };
+            spyOn(callbacks, 'fn').andCallThrough();
+        });
+
+        it('tests doCallback for a function that exists', function() {
+            AddressGadget.prototype.doCallback(
+                callbacks,
+                'fn',
+                'X',
+                'Y'
+            );
+
+            expect(callbacks.fn).toHaveBeenCalled();
+            expect(callbacks.fn.calls.length).toEqual(1);
+            expect(callbacks.fn).toHaveBeenCalledWith('X', 'Y');
+        });
+
+        it('tests doCallback for a function that does not exist', function() {
+            AddressGadget.prototype.doCallback(
+                callbacks,
+                'xxfn',
+                'X',
+                'Y'
+            );
+            expect(callbacks.fn).not.toHaveBeenCalled();
+        });
+    });
+});
+
+function lookupPostalCode(gc, postalCode, expectedAddr) {
+}
+
+describe('AddressGadgetGMap', function() {
+    it('tests that Google Maps dependency exists', function() {
+        expect(google.maps.Geocoder).not.toBeNull();
+    });
+
+    it('includeRoute', function() {
+        var gc = new AddressGadgetGMap();
+        expect(gc.includeRoute('au')).toBe(false);
+        expect(gc.includeRoute('ca')).toBe(true);
+        expect(gc.includeRoute('uk')).toBe(true);
+        expect(gc.includeRoute('us')).toBe(false);
+    });
+
+    testAllLookupPostalCode(new AddressGadgetGMap());
+});
+
+function testAllLookupPostalCode(gc) {
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: 'W1B 3BR',
+            countryCode: 'uk'
+        },
+        {
+            lat: 51.5142181,
+            lon: -0.14096349999999802,
+            locality: 'London',
+            tlaal: null,
+            tlaas: null,
+            postalCode: 'W1B 3BR',
+            countryCode: 'uk',
+            streetNumber: null,
+            route: null,
+            neighborhood: null
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: 'W1D 5JL',
+            countryCode: 'uk'
+        },
+        {
+            lat: 51.5132039,
+            lon: -0.13090199999999186,
+            locality: 'London',
+            tlaal: null,
+            tlaas: null,
+            postalCode: 'W1D 5JL',
+            countryCode: 'uk',
+            streetNumber: '23-25',
+            route: 'Old Compton Street',
+            neighborhood: null
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: 'V6R 2B2',
+            countryCode: 'ca'
+        },
+        {
+            lat: 49.2642568,
+            lon: -123.17937890000002,
+            locality: 'Vancouver',
+            tlaal: 'British Columbia',
+            tlaas: 'BC',
+            postalCode: 'V6R 2B2',
+            countryCode: 'ca',
+            streetNumber: '3338-3396',
+            route: 'West Broadway',
+            neighborhood: 'Kitsilano'
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: 'M4W 2H2',
+            countryCode: 'ca'
+        },
+        {
+            lat: 43.6740068,
+            lon: -79.3880418,
+            locality: 'Toronto',
+            tlaal: 'Ontario',
+            tlaas: 'ON',
+            postalCode: 'M4W 2H2',
+            countryCode: 'ca',
+            streetNumber: '869',
+            route: 'Yonge Street',
+            neighborhood: 'Rosedale'
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: '90210',
+            countryCode: 'us'
+        },
+        {
+            lat: 34.102898,
+            lon: -118.40889300000003,
+            locality: 'Beverly Hills',
+            tlaal: 'California',
+            tlaas: 'CA',
+            postalCode: '90210',
+            countryCode: 'us'
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: '92120',
+            countryCode: 'us'
+        },
+        {
+            lat: 32.7931503,
+            lon: -117.07345599999996,
+            locality: 'San Diego',
+            tlaal: 'California',
+            tlaas: 'CA',
+            postalCode: '92120',
+            countryCode: 'us'
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: '4113',
+            countryCode: 'au'
+        },
+        {
+            lat: -27.589278,
+            lon: 153.079161,
+            locality: 'Runcorn',
+            tlaal: 'Queensland',
+            tlaas: 'QLD',
+            postalCode: '4113',
+            countryCode: 'au'
+        }
+    );
+    testLookupPostalCode(
+        gc,
+        {
+            postalCode: '3188',
+            countryCode: 'au'
+        },
+        {
+            lat: -37.939468,
+            lon: 145.013643,
+            locality: 'Hampton',
+            tlaal: 'Victoria',
+            tlaas: 'VIC',
+            postalCode: '3188',
+            countryCode: 'au'
+        }
+    );
+}
+
+function testLookupPostalCode(gc, addr, expectedAddr) {
+    describe('lookupPostalCode', function() {
+        var _res, _addr, flag;
+
+        it('performs a lookup by postal code for: ' + addr.postalCode, function() {
+            // perform lookup
+            runs(function() {
+                gc.lookupPostalCode(
+                    addr,
+                    {
+                        onFullAddressMatch: function(res, addr) {
+                            _res = res;
+                            _addr = addr;
+                            done = true;
+                        },
+                        onNoMatch: function(res, addr) {
+                            _res = res;
+                            _addr = addr;
+                            done = true;
+                        }
+                    });
+            });
+
+            // wait for async call to complete
+            waitsFor(function() {
+                return _res;
+            }, 'res and addr should be set', 1500);
+
+            // test result
+            runs(function() {
+                expect(_res).not.toBeNull();
+                expect(_res.code).toEqual('SUCCESS');
+                expect(_res.exec_time).toBeGreaterThan(0);
+                for (var attr in expectedAddr) {
+                    expect(_addr[attr]).toBeDefined('Attribute [' + attr + '] doesn\'t exist in returned struct.');
+                    expect(_addr[attr]).toEqual(expectedAddr[attr]);
+                }
+            });
+
+            // delay before next test or the mapping API rejects the requests
+            runs(function() {
+                flag = false;
+                setTimeout(function() {
+                    flag = true;
+                }, 2000);
+            });
+            waitsFor(function() {
+                return flag;
+            }, "short delay before the next test", 2000);
+        });
+    });
+}
